3da384d0456acf2e370dd361e7e3010d454beb33
Joshua Martin <joshua@emiimaging.com>
2012-09-12 14:07:37 -0400

    Fix whitelist for password and confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   # accessibility
-  attr_accessible :full_name, :password_digest, :username
+  attr_accessible :full_name, :password, :password_confirmation, :username
 
   has_secure_password
   validates_presence_of :password, :on => :create
