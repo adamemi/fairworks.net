4d30a3b63ff31ca0ec21183e295bdf2df15470ef
Joshua Martin <joshua@emiimaging.com>
2012-09-16 22:20:18 -0400

    Deploying to 66.37.67.153

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,9 +15,9 @@ set :user,              "emi" # your server username
 set :deploy_to,         "/home/emi/webapps/#{application}"
 set :torquebox_home,    "/home/emi/torquebox-current"
 
-role :web,              "192.168.100.21" # location of the web server
-role :app,              "192.168.100.21" # may be the same as your 'Web' server
-role :db,               "192.168.100.21", :primary => true # where Rails migrations will run, NOT the location of the db
+role :web,              "66.37.67.153" # location of the web server
+role :app,              "66.37.67.153" # may be the same as your 'Web' server
+role :db,               "66.37.67.153", :primary => true # where Rails migrations will run, NOT the location of the db
 
 # necesary or else a '/usr/bin/env: jruby: No such file or directory' will occur
 set :default_environment, {
